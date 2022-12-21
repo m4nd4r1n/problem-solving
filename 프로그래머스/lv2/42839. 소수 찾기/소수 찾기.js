71cc2016fe3a02033c91bb011fb0883af8295bc3
Kim Do Hyeong <kara.mandarin99@gmail.com>
2022-12-21 16:54:43 +0900

    [level 2] Title: 소수 찾기, Time: 1.30 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,32 @@
+const solution = (numbers) => {
+  const papers = numbers.split("");
+  let count = 0;
+  new Set(
+    Array(numbers.length)
+      .fill()
+      .flatMap((_, index) =>
+        permutation(papers, index + 1).map((number) => +number.join(""))
+      )
+  ).forEach((number) => {
+    if (number < 2) return;
+    if (number % 2 === 0) {
+      if (number === 2) count += 1;
+      return;
+    }
+    for (let index = 3; index <= Math.sqrt(number); index += 2) {
+      if (number % index === 0) return;
+    }
+    count += 1;
+  });
+  return count;
+};
+
+const permutation = (array, length) => {
+  if (length === 1) return array.map((value) => [value]);
+  return array.flatMap((choice, index, origin) =>
+    permutation(
+      origin.filter((_, idx) => idx !== index),
+      length - 1
+    ).map((value) => [choice, ...value])
+  );
+};
