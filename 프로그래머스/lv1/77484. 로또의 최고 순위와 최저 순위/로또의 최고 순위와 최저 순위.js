344937fcba11ec4f0c4e7ee98ee19b9c3b04600d
m4nd4r1n <kara.mandarin99@gmail.com>
2023-01-15 11:04:00 +0000

    Update 로또의 최고 순위와 최저 순위.js

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,26 +1,8 @@
-function winCalculator(num) {
-  switch (num) {
-    case 6:
-      return 1;
-    case 5:
-      return 2;
-    case 4:
-      return 3;
-    case 3:
-      return 4;
-    case 2:
-      return 5;
-    default:
-      return 6;
-  }
-}
-
-function solution(lottos, win_nums) {
-  const answer = [];
+const solution = (lottos, win_nums) => {
+  const calculator = [6, 6, 5, 4, 3, 2, 1];
   const zeros = lottos.filter((x) => x === 0);
   const wins = lottos.filter((x) => win_nums.includes(x));
-  const max = winCalculator(wins.length + zeros.length);
-  const min = winCalculator(wins.length);
-  answer.push(max, min);
-  return answer;
-}
+  const max = calculator[wins.length + zeros.length];
+  const min = calculator[wins.length];
+  return [max, min];
+};
