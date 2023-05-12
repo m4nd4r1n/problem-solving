9fcb88cfe246045a77413ee2523b43685d822616
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-05-12 13:01:00 +0900

    [HackerRank] Permuting Two Arrays

---
+++ HackerRank/Interview Preparation Kit/1 Month Preparation Kit/10. Permuting Two Arrays/Permuting Two Arrays.js
@@ -0,0 +1,17 @@
+/*
+ * Complete the 'twoArrays' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ */
+
+function twoArrays(k, A, B) {
+  // Write your code here
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+  if (A.every((a, index) => a + B[index] >= k)) return "YES";
+  else return "NO";
+}
---
+++ HackerRank/Interview Preparation Kit/1 Month Preparation Kit/10. Permuting Two Arrays/README.md
@@ -0,0 +1,3 @@
+# Permuting Two Arrays
+
+[문제 링크](https://www.hackerrank.com/challenges/one-month-preparation-kit-two-arrays/problem)
