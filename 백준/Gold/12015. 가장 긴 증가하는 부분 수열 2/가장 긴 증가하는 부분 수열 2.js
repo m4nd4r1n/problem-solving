fa535d4d551b8b23d46cb5f3e09d28ee8d223c7a
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-08-28 23:35:57 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 948 ms, Memory: 163412 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  const N = Number(input[0]);
+  const sequence = input[1].split(' ').map(Number);
+
+  console.log(solution(N, sequence));
+});
+
+const solution = (N, sequence) => {
+  const LIS = [sequence[0], ...Array(N - 1).fill(0)];
+  let last = 0;
+
+  for (let i = 1; i < N; i++) {
+    if (sequence[i] > LIS[last]) {
+      last += 1;
+      LIS[last] = sequence[i];
+    } else {
+      let left = 0;
+      let right = last;
+
+      while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (LIS[mid] >= sequence[i]) {
+          right = mid;
+        } else {
+          left = mid + 1;
+        }
+      }
+
+      LIS[right] = sequence[i];
+    }
+  }
+
+  return last + 1;
+};
