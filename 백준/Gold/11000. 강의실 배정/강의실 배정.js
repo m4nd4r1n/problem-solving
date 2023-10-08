645a8d565d7ef2a559c435d8f114835f6587de72
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-08 17:08:11 +0900

    [Gold V] Title: 강의실 배정, Time: 696 ms, Memory: 83708 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.js
@@ -0,0 +1,85 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  const [N] = input[0].split(' ').map(Number);
+  const times = input.slice(1).map((v) => v.split(' ').map(Number));
+
+  console.log(solution(N, times));
+});
+
+const solution = (N, times) => {
+  const pq = new PriorityQueue((a, b) => a < b);
+  times.sort((a, b) => a[0] - b[0]);
+  pq.push(times[0][1]);
+  for (const [start, end] of times.slice(1)) {
+    if (pq.top() <= start) {
+      pq.pop();
+    }
+    pq.push(end);
+  }
+
+  return pq.size();
+};
+
+class PriorityQueue {
+  constructor(compare) {
+    this.heap = [null];
+    this.compare = compare;
+  }
+
+  push(item) {
+    this.heap.push(item);
+    let currentNode = this.heap.length - 1;
+    let parentNode = Math.floor(currentNode / 2);
+
+    while (parentNode !== 0 && this.compare(this.heap[currentNode], this.heap[parentNode])) {
+      this._swap(parentNode, currentNode);
+      currentNode = parentNode;
+      parentNode = Math.floor(currentNode / 2);
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 1) return;
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const top = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let [currentNode, leftChild, rightChild] = [1, 2, 3];
+
+    while (
+      this.compare(this.heap[leftChild], this.heap[currentNode]) ||
+      this.compare(this.heap[rightChild], this.heap[currentNode])
+    ) {
+      const targetNode = this.compare(this.heap[rightChild], this.heap[leftChild])
+        ? rightChild
+        : leftChild;
+      this._swap(currentNode, targetNode);
+      currentNode = targetNode;
+      leftChild = currentNode * 2;
+      rightChild = currentNode * 2 + 1;
+    }
+
+    return top;
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+
+  top() {
+    return this.heap[1];
+  }
+
+  _swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+}
