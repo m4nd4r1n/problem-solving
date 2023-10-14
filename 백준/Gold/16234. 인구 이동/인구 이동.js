c14b1f1f32e25c1356d1ca16d6a55ef425508156
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-10-15 01:46:43 +0900

    [Gold IV] Title: 인구 이동, Time: 4932 ms, Memory: 15428 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.js
@@ -0,0 +1,99 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line);
+}).on('close', () => {
+  const [N, L, R] = input[0].split(' ').map(Number);
+  const map = input.slice(1).map((v) => v.split(' ').map(Number));
+
+  console.log(solution(N, L, R, map));
+});
+
+const solution = (N, L, R, map) => {
+  const tempMap = map.map((row) => [...row]);
+  const visited = Array.from(Array(N), () => Array(N).fill(false));
+  const dx = [0, 0, -1, 1];
+  const dy = [-1, 1, 0, 0];
+  let count = 0;
+  let isMoved = false;
+
+  const bfs = (x, y) => {
+    const queue = new Queue();
+    const unions = [];
+    let sum = 0;
+
+    queue.push([x, y]);
+    visited[x][y] = true;
+
+    while (!queue.isEmpty()) {
+      const [curX, curY] = queue.pop();
+      sum += map[curX][curY];
+      unions.push([curX, curY]);
+
+      for (let i = 0; i < 4; i++) {
+        const nx = curX + dx[i];
+        const ny = curY + dy[i];
+
+        if (nx >= 0 && nx < N && nx >= 0 && ny < N && !visited[nx][ny]) {
+          const diff = Math.abs(map[curX][curY] - map[nx][ny]);
+          if (diff >= L && diff <= R) {
+            queue.push([nx, ny]);
+            visited[nx][ny] = true;
+            isMoved = true;
+          }
+        }
+      }
+    }
+
+    const divide = Math.floor(sum / unions.length);
+    for (const [x, y] of unions) {
+      tempMap[x][y] = divide;
+    }
+  };
+
+  while (true) {
+    isMoved = false;
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        if (!visited[i][j]) {
+          bfs(i, j);
+        }
+      }
+    }
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        visited[i][j] = false;
+        map[i][j] = tempMap[i][j];
+      }
+    }
+    if (!isMoved) return count;
+    count += 1;
+  }
+};
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  push(item) {
+    this.queue[this.rear++] = item;
+  }
+
+  pop() {
+    const item = this.queue[this.front];
+    delete this.queue[this.front++];
+    return item;
+  }
+
+  isEmpty() {
+    return this.front === this.rear;
+  }
+}
